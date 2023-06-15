ffd74bb260dfd90241ff6759f3a84ff515fe9d33
aptekkkar <42844469+aptekkkar@users.noreply.github.com>
2023-06-15 21:04:21 +0300

    Update Object.h

--- 3_1_2/Object.h
+++ 3_1_2/Object.h
@@ -1,1 +1,17 @@
+#ifndef _OBJECT_H_
+#define _OBJECT_H_
 
+#include <iostream>
+
+class Object
+{
+public:
+	Object(int);
+
+	void show_massive();
+	void update_massive(int);
+private:
+	int* massive, size;
+};
+
+#endif
