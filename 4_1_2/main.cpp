83a1400c928efaac67a11a0e701dc32df3eba732
aptekkkar <42844469+aptekkkar@users.noreply.github.com>
2023-06-15 23:02:53 +0300

    Update main.cpp

--- 4_1_2/main.cpp
+++ 4_1_2/main.cpp
@@ -1,1 +1,20 @@
+#include "cl_object_4.h"
 
+int main()
+{
+	int number;
+	std::string identifier;
+	std::cin >> identifier >> number;
+	
+	if (number >= 2 && number <= 10)
+	{
+		cl_object_4 object(identifier, number);
+		((cl_object_1)object).show_object();
+		((cl_object_2)object).show_object();
+		((cl_object_3)object).show_object();
+		((cl_object_4)object).show_object();
+
+		return 0;
+	}
+	return 1;
+}
