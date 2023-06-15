d2c693d9a3e0e751e63f20d77356e365130f5b76
aptekkkar <42844469+aptekkkar@users.noreply.github.com>
2023-06-15 21:17:32 +0300

    Update main.cpp

--- 3_1_3/main.cpp
+++ 3_1_3/main.cpp
@@ -1,1 +1,28 @@
+#include "Object.h"
 
+void function(Object object)
+{
+	std::cout << object.multi_pairs() << std::endl;
+}
+
+int main()
+{
+	int size; 
+	std::cin >> size;
+
+	if (size > 2 && size % 2 == 0)
+	{
+		std::cout << size << std::endl;
+
+		Object object(size);
+		object.input_massive();
+		function(object);
+		object.sum_pairs();
+		std::cout << std::endl << object.get_sum() << std::endl;
+
+		return 0;
+	}
+	std::cout << size << "?";
+
+	return 1;
+}
