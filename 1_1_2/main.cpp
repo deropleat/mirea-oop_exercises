a5f7e77b5efb1980a010a0601e9a667454c0eef7
aptekkkar <42844469+aptekkkar@users.noreply.github.com>
2023-06-15 19:54:05 +0300

    Update main.cpp

--- 1_1_2/main.cpp
+++ 1_1_2/main.cpp
@@ -1,1 +1,16 @@
+#include <iostream>
 
+int main()
+{
+    int n, Sum_n = 0;
+    std::cin >> n;
+
+    if (n > 0)
+    {
+        for (int i = 1; i <= n; i++) Sum_n = Sum_n + i;
+        std::cout << "Sum_n = " << Sum_n;
+    }
+    else std::cout << "n is wrong (" << n << ")";
+
+    return 0;
+}
