6b854221c5d6053177fd60d7c114b24d144dc3b4
aptekkkar <42844469+aptekkkar@users.noreply.github.com>
2023-06-15 22:52:01 +0300

    Update main.cpp

--- 4_1_1/main.cpp
+++ 4_1_1/main.cpp
@@ -1,1 +1,32 @@
+#include "cl_children.h"
 
+int main()
+{
+	int privateInt, publicInt;
+	std::cin >> privateInt >> publicInt;
+	cl_children children(privateInt, publicInt);
+	cl_parent parent = (cl_parent)children;
+	parent.show_integers();
+	std::cout << std::endl;
+	children.show_integers();
+	std::cout << std::endl;
+	
+	if (privateInt > 0)
+	{
+		children.change_integers(privateInt + 1, publicInt + 1);
+		parent.change_integers(privateInt - 1, publicInt - 1);
+		children.show_integers();
+		std::cout << std::endl;
+		parent.show_integers();
+	}
+	else
+	{
+		parent.change_integers(privateInt + 1, publicInt + 1);
+		children.change_integers(privateInt - 1, publicInt - 1);
+		parent.show_integers();
+		std::cout << std::endl;
+		children.show_integers();
+	}
+
+	return 0;
+}
