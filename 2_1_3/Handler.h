2132f69eb6688416d80633fc293e2c936d441144
aptekkkar <42844469+aptekkkar@users.noreply.github.com>
2023-06-15 20:28:18 +0300

    Update Handler.h

--- 2_1_3/Handler.h
+++ 2_1_3/Handler.h
@@ -1,1 +1,19 @@
+#ifndef _HANDLER_H_
+#define _HANDLER_H_
 
+#include <iostream>
+#include <string>
+
+class Handler
+{
+public:
+	Handler();
+private:
+	void writeIn();
+	void readFrom();
+	void inReverse();
+
+	int* M = nullptr, N = 0;
+};
+
+#endif
