3521327ac77e4100412d19f519d3254c1639c273
aptekkkar <42844469+aptekkkar@users.noreply.github.com>
2023-06-15 19:58:21 +0300

    Update main.cpp

--- 1_1_3/main.cpp
+++ 1_1_3/main.cpp
@@ -1,1 +1,13 @@
+#include <iostream>
 
+int main()
+{
+    int length = 1;
+    long long int num;
+    std::cin >> num;
+
+    for (; abs(num) >= 10; num /= 10) length++;
+    std::cout << "Number of digits: " << length;
+
+    return(0);
+}
