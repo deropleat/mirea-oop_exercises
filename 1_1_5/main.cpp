986627be9cebadf7a7c58e3ef803008721837120
aptekkkar <42844469+aptekkkar@users.noreply.github.com>
2023-06-15 20:02:35 +0300

    Update main.cpp

--- 1_1_5/main.cpp
+++ 1_1_5/main.cpp
@@ -1,1 +1,23 @@
+#include <iostream>
 
+int main()
+{
+	int N;
+	std::cin >> N;
+	
+	if (N >= 1 && N <= 9)
+	{
+		for (int i = 1; i <= N; i++)
+		{
+			for (int j = 1; j <= i; j++)
+			{
+				std::cout << j;
+				if (j < i) std::cout << " ";
+			}
+			if (i < N) std::cout << std::endl;
+		}
+	}
+	else std::cout << "N is wrong: " << N;
+
+	return 0;
+}
