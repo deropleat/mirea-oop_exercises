0847ff5e86e911194af4841359524b106386bf10
aptekkkar <42844469+aptekkkar@users.noreply.github.com>
2023-06-15 20:16:33 +0300

    Update main.cpp

--- 2_1_2c/main.cpp
+++ 2_1_2c/main.cpp
@@ -1,1 +1,14 @@
+#include "Triangle.h"
 
+int main()
+{
+	int side[3] = { 0, 0, 0 };
+	for (int i = 0; i < 3; i++) while (side[i] <= 0) std::cin >> side[i];
+	
+	Triangle* triangle = new Triangle(side[0], side[1], side[2]);
+	std::cout << "P = " << triangle->getObjectSize() << std::endl;
+	std::cout << "S = " << triangle->getObjectSquare();
+	delete triangle;
+
+	return 0;
+}
