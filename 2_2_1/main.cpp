a12771c7b781ae6f20684e3f1aa595a4f7ff8153
aptekkkar <42844469+aptekkkar@users.noreply.github.com>
2023-06-15 20:47:03 +0300

    Update main.cpp

--- 2_2_1/main.cpp
+++ 2_2_1/main.cpp
@@ -1,1 +1,26 @@
+#include "Math.h"
 
+int main()
+{
+	int data;
+	std::cin >> data;
+	
+	Math* math = new Math(data);
+	math->read_state();
+	std::cout << std::endl;
+
+	math->change_state();
+	math->read_state();
+	std::cout << std::endl;
+
+	std::cin >> data;
+	math->available *= data;
+	math->read_state();
+	std::cout << std::endl;
+
+	math->call_private();
+	math->read_state();
+	delete math;
+
+	return 0;
+}
